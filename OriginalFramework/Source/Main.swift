a725d891092023206edb9ad68b41f03e2d4e4cf7
ngo275 <ngo38114@gmail.com>
2019-10-24 15:18:30 +0900

    Updated podspec

--- OriginalFramework/Source/Main.swift
+++ OriginalFramework/Source/Main.swift
@@ -11,14 +11,27 @@ import UIKit
 public struct TestFramework {
     public static func getVC() -> UINavigationController {
         UIFont.loadMyFonts
+        setNavigationBar()
         return LoginViewController.instantiate()
     }
     
     public static func start() {
         UIFont.loadMyFonts
+        setNavigationBar()
         let vc = LoginViewController.instantiate()
         let window = UIApplication.shared.keyWindow
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
+    }
+    
+    // MARK: - Private methods
+    
+    private static func setNavigationBar() {
+        let navigationBarAppearace = UINavigationBar.appearance()
+        navigationBarAppearace.largeTitleTextAttributes = [NSAttributedString.Key.font: UIFont.abeatbyKai(ofSize: 34), NSAttributedString.Key.foregroundColor: UIColor.ex.white]
+        navigationBarAppearace.titleTextAttributes = [NSAttributedString.Key.font: UIFont.abeatbyKai()]
+        navigationBarAppearace.tintColor = UIColor.ex.white
+        navigationBarAppearace.barTintColor = UIColor.ex.backgroundDark
+        navigationBarAppearace.isTranslucent = false
     }
 }
