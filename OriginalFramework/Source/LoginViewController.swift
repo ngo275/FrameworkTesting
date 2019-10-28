44aebbc40b9bb7aed841cc730dc2aa09b408772d
ngo275 <ngo38114@gmail.com>
2019-10-28 10:43:16 +0900

    Added git submodule

--- OriginalFramework/Source/LoginViewController.swift
+++ OriginalFramework/Source/LoginViewController.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import BLTNBoard
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var imageView: UIImageView!
+    
+    var bulletinManager: BLTNItemManager?
     
     static func instantiate() -> UINavigationController {
         let sb = UIStoryboard(name: "Login", bundle: Bundle(for: LoginViewController.self))
         let vc = sb.instantiateInitialViewController() as! UINavigationController
         return vc
+    }
+    
+    private func makeBulletinManager() -> BLTNItemManager {
+        let page = BLTNPageItem(title: "Title!!")
+        page.image = UIImage(named: "logo_credify")
+        
+        let appearance = BLTNItemAppearance()
+        appearance.titleFontDescriptor = UIFont.abeatbyKai().fontDescriptor
+        appearance.descriptionFontDescriptor = UIFont.quicksand().fontDescriptor
+        appearance.buttonFontDescriptor = UIFont.quicksand().fontDescriptor
+        page.appearance = appearance
+        page.isDismissable = false
+        page.requiresCloseButton = false
+        // page.descriptionText = String(format: "PendingTxDescription".localized(tableName: tn), presenter.pendingField)
+        page.descriptionText = "Some Description"
+        page.actionButtonTitle = "Execute"
+        page.actionHandler = { item in
+            self.goNext()
+        }
+        
+        let manager = BLTNItemManager(rootItem: page)
+        return manager
     }
     
     override func viewDidLoad() {
@@ -35,6 +60,11 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func tapButton(_ sender: Any) {
+        self.bulletinManager = self.makeBulletinManager()
+        self.bulletinManager?.showBulletin(in: UIApplication.shared)
+    }
+    
+    func goNext() {
         let vc = InputViewController.instantiate()
         navigationController?.pushViewController(vc, animated: true)
     }
